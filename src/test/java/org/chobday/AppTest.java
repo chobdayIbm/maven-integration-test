82682bb2bba2a5a99074e2e3b0fa56d7b918bb14
Colin Hobday <chobday@sg.ibm.com>
2020-06-15 18:26:45 +0800

    post works

--- src/test/java/org/chobday/AppTest.java
+++ src/test/java/org/chobday/AppTest.java
@@ -9,6 +9,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class AppTest 
 {
@@ -16,6 +21,8 @@ public class AppTest
     private final HttpClient httpClient = HttpClient.newBuilder()
     .version(HttpClient.Version.HTTP_2)
     .build();
+
+    private final JSONParser parser = new JSONParser();
 
     @Test
     @DisplayName("Canary Test")
@@ -25,20 +32,36 @@ public class AppTest
     }
 
     @Test
-    @DisplayName("Test Api")
-    void testApi()
+    @DisplayName("Test Get")
+    void testGet() throws Exception
     {
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://httpbin.org/get"))
-                .setHeader("User-Agent", "Java 11 HttpClient Bot")
-                .build();
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            assertEquals(200, response.statusCode());
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        HttpRequest request = HttpRequest.newBuilder()
+            .GET()
+            .uri(URI.create("https://httpbin.org/get"))
+            .setHeader("User-Agent", "Java 11 HttpClient Bot")
+            .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+        
+        
+        JSONObject json = (JSONObject) parser.parse(response.body());
+        assertEquals("218.212.245.238", json.get("origin"));
+    }
+
+    @Test
+    @DisplayName("Test Post")
+    void testPost() throws Exception {
+        String testUrl = "https://httpbin.org/post";
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(testUrl))
+            .setHeader("User-Agent", "Java 11 HttpClient Bot")
+            .POST(BodyPublishers.ofString(""))
+            .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+        
+        JSONObject json = (JSONObject) parser.parse(response.body());
+        assertEquals("218.212.245.238", json.get("origin"));
     }
 
 }
